2b63369743c78f608da094532fd36e03a95666fd
杨汶珑的PC <965972416@qq.com>
2020-12-01 21:43:09 +0800

    框架结构搭建

---
+++ src/main/java/com/ywltest/springdemo/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ywltest.springdemo.service.impl;
+
+import com.ywltest.springdemo.service.UserService;
+
+/**
+ * @author yangWenlong
+ * @date 2020/12/1- ${time}
+ */
+public class UserServiceImpl implements UserService {
+}
