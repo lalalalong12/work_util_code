3506d3fed1034fe5529fbd659663f7996dcae170
杨汶珑的PC <965972416@qq.com>
2020-12-01 21:07:47 +0800

    初始化

---
+++ src/main/java/com/ywltest/springdemo/SpringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ywltest.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdemoApplication.class, args);
+    }
+
+}
