7ed17fff870e66b61e0d9bea9499cc33ac94dd8a
yangwenlong <yangwenlongb@enn.cn>
2020-12-03 09:14:07 +0800

    打印地址

---
+++ src/main/java/com/ywltest/springdemo/config/ApplicationRunnerImpl.java
@@ -0,0 +1,31 @@
+package com.ywltest.springdemo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author yangWenlong
+ * @date 2020/12/1- ${time}
+ */
+@Component
+public class ApplicationRunnerImpl implements ApplicationRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(ApplicationRunnerImpl.class);
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("application start...");
+
+        try {
+            log.info("\n----------------------------------------------------------\n\tApplication '{}' is running! Access URLs:\n\tLocal: \t\t{}://localhost:{}\n\tExternal: \t{}://{}:{}\n\tProfile(s): \t{}\n----------------------------------------------------------", "spring.application.name", "protocol", "server.port", "protocol", InetAddress.getLocalHost().getHostAddress(), "server.port", "env.getActiveProfiles()");
+        } catch (UnknownHostException var8) {
+            var8.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/ywltest/springdemo/config/Config.java
+++
@@ -1,8 +0,0 @@
-package com.ywltest.springdemo.config;
-
-/**
- * @author yangWenlong
- * @date 2020/12/1- ${time}
- */
-public class Config {
-}
